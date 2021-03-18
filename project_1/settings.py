abd9c52e8f4cdecf94bdda8a884203a19b0e4d41
Michael Tobis <michaeltobis@Michaels-MBP.phub.net.cable.rogers.com>
2021-03-18 11:46:04 -0400

    updated settings files

--- project_1/settings.py
+++ project_1/settings.py
@@ -126,6 +126,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_DIRS = {
+    os.path.join(BASE_DIR, 'frontend/static')
+}
+
 AUTH_USER_MODEL = 'chat_app.User'
 
 REST_FRAMEWORK = {
