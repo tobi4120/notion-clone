31b2dc9782efa92c36769093d95661e546a2e051
Michael Tobis <tobi4120@mylaurier.ca>
2021-04-04 16:50:50 -0400

    changing database

--- project_1/settings.py
+++ project_1/settings.py
@@ -88,12 +88,8 @@ WSGI_APPLICATION = 'project_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ['database_name'],
-        'USER': os.environ['database_user'],
-        'PASSWORD': os.environ['database_password'],
-        'HOST': 'database-1.cnjiipmf4v9e.us-east-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
