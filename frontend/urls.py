37bf7a41206d54af91a5ef33c46c6f211702f1cf
Michael Tobis <tobi4120@mylaurier.ca>
2022-10-11 22:47:26 -0400

    modified url so that admin url redirects to admin page

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .import views
+from django.contrib import admin
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('login', views.index, name="index"),
     path('register', views.index, name="index"),
